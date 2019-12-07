801e3822d1e5844124683be5639ee57da3c5de58
Asad Shabir <47941595+Asad520@users.noreply.github.com>
2019-12-07 14:25:08 +0500

    Add files via upload

---
+++ ejs.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+var app = express();
+app.set("view engine", "ejs");
+app.use("/style", express.static("styles"));
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+app.get("/contact", (req, res) => {
+  res.render("pages/contact");
+});
+app.get("/profile/:name", (req, res) => {
+  var data = {
+    age: 21,
+    job: "Developer",
+    hobbies: ["Over Thinking", "Eating", "Sleeping"]
+  };
+  res.render("/pages/profile", { person: req.params.name, data: data });
+});
+
+app.listen(3000);
+console.log("LISTENING 3000");
